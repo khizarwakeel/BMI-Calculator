aa9f5d63eaa73b5cd53672fccc3663ade00bcc20
Khizar Wakeel <khizarwakeel@gmail.com>
2024-09-17 03:32:12 +0500

    add BMI Complete Logic and Design

--- App.js
+++ App.js
@@ -8,21 +8,41 @@ function calculateBMI(event) {
 
     heightInputInch += heightInputFeet * 12;
 
-    let heightInMeter = (heightInputInch * 2.54) / 10;
+    let heightInMeter = (heightInputInch * 2.54) / 100;
     let heightInMeterSq = heightInMeter * heightInMeter;
-    let bodyMassIndex = weightInputKg / heightInMeterSq;
+    let bodyMassIndex = (weightInputKg / heightInMeterSq).toFixed(2);
+    let idealWeight = (22 * heightInMeterSq).toFixed(2);
 
-    let title = "";
+    let weightToLose = (weightInputKg - idealWeight).toFixed(2);
+    let weightToGain = (idealWeight - weightInputKg).toFixed(2);
+
+    let title = "You are ";
     if (bodyMassIndex > 30) {
-        title = "obese";
+        title += `Obese. you need to lose ${weightToLose}kg`;
     }
     else if (bodyMassIndex > 25) {
-        title = "Overweight";
+        title += `Overweight. you need to lose ${weightToLose}kg`;
     }
     else if (bodyMassIndex > 18.5) {
-        title = "Normal";
+        if (bodyMassIndex > 22) {
+            title += `Normal but optionally you may lose ${weightToLose}kg to become ideally fit`;
+        }
+        else if (parseInt(bodyMassIndex) === 22) {
+            title += `perfectly Fit`;
+        }
+        else {
+            title += `Normal but optionally you may gain ${weightToGain}kg to become ideally fit`;
+        }
     }
     else {
-        title = "Underweight";
+        title += `Underweight you are supposed to gain ${weightToGain}kg to become normal`;
     }
+
+    console.log("BMI Index =", bodyMassIndex);
+    console.log("Title =", title);
+    console.log("Ideal Weight =", idealWeight + "kg");
+    document.querySelector("#BMIIndex").innerHTML = "<span style='font-weight: bold;'> BMI Index :</span> " + bodyMassIndex;
+    document.querySelector("#title").innerHTML = "<span style='font-weight: bold;'> Guide :</span> " + title;
+    document.querySelector("#averageWeight").innerHTML = "<span style='font-weight: bold;'> Ideal Weight :</span> " + idealWeight;
+
 }
